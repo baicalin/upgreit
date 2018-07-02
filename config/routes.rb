22480225d68874dcbad179fc854df6f6974df1b3
EC2 Default User <ec2-user@ip-172-31-34-10.us-east-2.compute.internal>
2018-07-02 10:59:59 +0000

    Update; minor

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'main/about'
+  get 'main/search'
+  get 'main/dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
