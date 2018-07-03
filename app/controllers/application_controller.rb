f7a2d4d5a2150f418388771b4a6f3272067ddf89
handnson <heetae21@hanmail.net>
2018-07-03 16:19:29 +0000

    add sign up coulum

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    
+    before_action :configure_permitted_parameters, if: :devise_controller? 
+    
+    protected 
+    
+    
+    def configure_permitted_parameters 
+        
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:birth])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+     
+     
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:birth])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:phone_number])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+        
+    end 
+   
+
 end
--- db/migrate/20180703123406_devise_create_users.rb
+++ db/migrate/20180703123406_devise_create_users.rb
@@ -2,10 +2,17 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
+    
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :birth
+      t.string :phone_number,      null: false, default: ""
+      t.string :address,            null: false, default: ""
+      
+      
 
       ## Recoverable
       t.string   :reset_password_token
